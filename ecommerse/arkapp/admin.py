214679db1a960282f2d356b4e8bb4f72ba57cb55
mangesh vispute <mangesh2003vispute@gmail.com>
2023-11-02 22:18:06 +0530

    paytm integration is started

--- ecommerse/arkapp/admin.py
+++ ecommerse/arkapp/admin.py
@@ -1,7 +1,10 @@
 import arkapp
 from django.contrib import admin
 import django.db
-from .models import Product
+from .models import Orders, OrderUpdate
+from .models import Product,Orders,OrderUpdate
 
 # Register your models here.
 admin.site.register(Product)
+admin.site.register(Orders)
+admin.site.register(OrderUpdate)
--- ecommerse/arkapp/views.py
+++ ecommerse/arkapp/views.py
@@ -1,7 +1,8 @@
 import django.contrib.auth
 import django.contrib.auth.models
 from django.shortcuts import render
-from .models import Product
+from .models import Product,Orders
+from .models import OrderUpdate
 from math import ceil
 from django.contrib import messages
 from django.shortcuts import redirect
@@ -28,4 +29,45 @@ def checkout(request):
     if not request.user.is_authenticated:
         messages.warning(request,"login and try again")
         return redirect("/arkauth/login")
-    return render(request,'checkout.html')
+   
+    if request.method=="POST":
+
+        items_json = request.POST.get('itemsJson', '')
+        name = request.POST.get('name', '')
+        amount = request.POST.get('amt')
+        email = request.POST.get('email', '')
+        address1 = request.POST.get('address1', '')
+        address2 = request.POST.get('address2','')
+        city = request.POST.get('city', '')
+        state = request.POST.get('state', '')
+        zip_code = request.POST.get('zip_code', '')
+        phone = request.POST.get('phone', '')
+         
+
+        Order = Orders(items_json=items_json,name=name,amount=amount, email=email, address1=address1,address2=address2,city=city,state=state,zip_code=zip_code,phone=phone)
+        print(amount)
+        Order.save()
+        update = OrderUpdate(order_id=Order.order_id,update_desc="the order has been placed")
+        update.save()
+        thank = True
+        # payment integration
+        
+        id = Order.order_id
+        oid=str(id)+"SecureBuy"
+        oid=str(id)
+        param_dict = {
+
+            'MID': 'add ur merchant id',
+            'ORDER_ID': oid,
+            'TXN_AMOUNT': str(amount),
+            'CUST_ID': email,
+            'INDUSTRY_TYPE_ID': 'Retail',
+            'WEBSITE': 'WEBSTAGING',
+            'CHANNEL_ID': 'WEB',
+            'CALLBACK_URL': 'http://127.0.0.1:8000/handlerequest/',
+
+        }
+        param_dict['CHECKSUMHASH'] = Checksum.generate_checksum(param_dict, MERCHANT_KEY)
+        return render(request, 'paytm.html', {'param_dict': param_dict})
+
+    return render(request, 'checkout.html')
