ed47f8c3fd3b55814c04b7068b162a2bc73d23f0
mangesh vispute <mangesh2003vispute@gmail.com>
2023-11-05 18:58:22 +0530

    Database is changed to the mysql

--- ecommerse/ecommerse/settings.py
+++ ecommerse/ecommerse/settings.py
@@ -82,10 +82,16 @@ WSGI_APPLICATION = 'ecommerse.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ecommerce',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
